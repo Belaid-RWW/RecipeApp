8213a5a631cb2db1c965fee1cad62417d5e8e478
unknown <sabriabassi1993@gmail.com>
2020-12-05 11:09:57 +0100

    Adding Add-recipe Page

--- src/app/recipes/recipes-routing.module.ts
+++ src/app/recipes/recipes-routing.module.ts
@@ -11,7 +11,11 @@ const routes: Routes = [
   {
     path: 'recipe-detail',
     loadChildren: () => import('./recipe-detail/recipe-detail.module').then( m => m.RecipeDetailPageModule)
+  },  {
+    path: 'add-recipe',
+    loadChildren: () => import('./add-recipe/add-recipe.module').then( m => m.AddRecipePageModule)
   }
+
 ];
 
 @NgModule({
